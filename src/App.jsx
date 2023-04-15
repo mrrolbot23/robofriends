23cb0b32d8ec814b4f2f16b96757bce4350826b0
mrrolbot23 <roldandelgado@icloud.com>
2023-04-15 16:48:40 -0400

    add users API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -17,20 +22,24 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robots) => {
-      return robots.name
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <>
-        <div className="tc">
-          <h1>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
-        </div>
-      </>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">LOADING</h1>;
+    } else {
+      return (
+        <>
+          <div className="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <CardList robots={filteredRobots} />
+          </div>
+        </>
+      );
+    }
   }
 }
 
